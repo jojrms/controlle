086fab001e742acd1fc707f175660e0022b30cee
Jordana <jordana@spott.eco>
2023-07-13 23:51:00 -0300

    feat: Creating options in menu left

--- src/components/menuLeft/index.tsx
+++ src/components/menuLeft/index.tsx
@@ -2,15 +2,35 @@ import {
     Background,
     Icon,
     List,
+    Option,
     BottomOptions
 } from "./styled"
+
+import Dashboard from '../../images/menuLeft/Dashboard.svg';
+import Finance from '../../images/menuLeft/Finance.svg';
+import Settings from '../../images/menuLeft/Settings.svg';
 
 export const MenuLeft = () => {
 
     return(
         <Background>
             <Icon/>
-            <List></List>
+
+            <List>
+                <Option selected={true}>
+                    <img width={"24px"} height={"24px"} src={Dashboard} alt="Dashboard icon"/>
+                    Dashboard  
+                </Option>
+                <Option selected={false}>
+                    <img width={"24px"} height={"24px"} src={Finance} alt="Finance icon"/>
+                    Financeiro  
+                </Option>
+                <Option selected={false}>
+                    <img width={"24px"} height={"24px"} src={Settings} alt="Settings icon"/>
+                    Configurações  
+                </Option>
+            </List>
+
             <BottomOptions></BottomOptions>
         </Background>
     )
--- src/components/menuLeft/styled.ts
+++ src/components/menuLeft/styled.ts
@@ -1,5 +1,11 @@
 import styled from 'styled-components';
 
+import IconUrl from '../../images/menuLeft/Icon.svg'
+
+type OptionProps = {
+    selected: boolean;
+}
+
 export const Background = styled.aside`
     background-color: ${props => props.theme.backgroundColor.menuLeft};
     grid-area: menu;
@@ -11,11 +17,25 @@ export const Background = styled.aside`
 export const Icon = styled.i`
     width: 100%;
     height: 100%;
+    background-image: url(${IconUrl});
+    background-size: 50%;
 `
 
 export const List = styled.ul`
-    width: 100%;
-    height: 100%;
+    height: fit-content;
+    padding: 0px 16px;
+    display: grid;
+    grid-template-rows: min-content;
+    gap: 5px;
+`
+export const Option = styled.li<OptionProps>`
+    background-color: ${({selected}) => selected ? props => props.theme.backgroundColor.backgroundIcons : "transparent"};
+    color: ${({selected}) => selected ? props => props.theme.fontColor.menuLeft.activeOption : props => props.theme.fontColor.menuLeft.inactiveOption};
+    display: flex;
+    align-items: center;
+    gap: 15px;
+    padding: 18px 20px;
+    border-radius: ${props => props.theme.border.menuBorderRadius}
 `
 
 export const BottomOptions = styled.div`
