75ac9c77921adbf15d937d92b5508758feec57ce
Jordana <jordana@spott.eco>
2023-07-13 23:05:35 -0300

    style: Adding colors in theme file

--- src/components/menuLeft/index.tsx
+++ src/components/menuLeft/index.tsx
@@ -4,7 +4,6 @@ export const MenuLeft = () => {
 
     return(
         <Background>
-            <button></button>
         </Background>
     )
 }
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,3 +1,25 @@
 export default {
-    fontColor: {}
+    fontColor: {
+        menuLeft: {
+            inactiveOption: '#949494',
+            activeOption: '#FFFFFF',
+        },
+        header: {
+            title: '#2D415F'
+        },
+        content: {
+            title: '#000000',
+            subtitle: '#9A9A9A'
+        }
+    },
+    backgroundColor: {
+        menuLeft: '#ECEDF3',
+        backgroundGeneral: '#F3F4F9',
+        cards: '#FFFFFF',
+        backgroundIcons: '#23272E',
+    },
+    border: {
+        borderColor: '#E1E4EB',
+        borderRadius: '12px',
+    }
 }
