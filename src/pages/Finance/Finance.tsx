1cdf1b79e52d6f526c39ac59ca361c1dba6afdeb
Jordana <jordanarmos@gmail.com>
2023-07-15 20:18:06 -0300

    feat: Applying table component in Finance page

--- src/pages/Finance/Finance.tsx
+++ src/pages/Finance/Finance.tsx
@@ -1,17 +1,64 @@
-import { InitialFilter, CreateNewFilter } from '../../components/filters'
+import React from 'react';
+import DataTable from 'react-data-table-component';
+
+import { InitialFilter, CreateNewFilter, Filter } from '../../components/filters'
 import {
-    Background, Container
-} from './styled'
+    Background, 
+    Container,
+    FiltersGrid
+} from './styled';
+
+import { Modal } from '../../components/modal';
 
 export const Finance = () => {
+
+    const [haveNewFilter, setHaveNewFilter] = React.useState<boolean>(false);
+    const [isModalOpen, setIsModalOpen] = React.useState<boolean>(false);
+
+    const columns = [
+        {
+            name: 'Title',
+            selector: (row: any) => row.title,
+        },
+        {
+            name: 'Year',
+            selector: (row: any) => row.year,
+        },
+    ];
+    
+    const data = [
+        {
+            id: 1,
+            title: 'Beetlejuice',
+            year: '1988',
+        },
+        {
+            id: 2,
+            title: 'Ghostbusters',
+            year: '1984',
+        },
+    ]
+
     return (
         <Background>
+
+            {isModalOpen && <Modal setIsModalOpen={setIsModalOpen}/>}
+
             <Container>
-                <InitialFilter/>
-                <CreateNewFilter/>
+                <FiltersGrid>
+                    <InitialFilter/>
+                    {/* <Filter label='Conta' MenuText='teste'/> */}
+                    <CreateNewFilter setIsModalOpen={setIsModalOpen}/>    
+                </FiltersGrid>
+                
                 <button id='buttonDeleteFilters'>Zerar filtros</button>
                 <button id='buttonSaveFilters'>Salvar filtros</button>
             </Container>
+
+            <DataTable
+            columns={columns}
+            data={data}
+            />
         </Background>
     )
 }
--- src/pages/Finance/styled.ts
+++ src/pages/Finance/styled.ts
@@ -4,16 +4,31 @@ export const Background = styled.section`
     background-color: ${props => props.theme.backgroundColor.backgroundGeneral};
     grid-area: content;
     display: grid;
+    grid-template-rows: min-content auto;
+    gap: 30px;
     border-bottom: 2px solid transparent;
     border-color: ${props => props.theme.border.borderColor};
+    padding: 0px 30px 0px 60px;
+
+    #buttonDeleteFilters{
+        margin: 0px 10px 0px auto;
+    }
 `
 
 export const Container = styled.div`
-    width: 100%;
-    height: 100px;
+    height: 80px;
     display: flex;
+    align-items: end;
 
     button{
         color: ${props => props.theme.fontColor.header.title};
     }
+`
+
+export const FiltersGrid = styled.div`
+    width: fit-content;
+    height: fit-content;
+    display: flex;
+    align-items: end;
+    gap: 10px;
 `
